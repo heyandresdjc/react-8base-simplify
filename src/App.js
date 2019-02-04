9822adbe77abb0714b727772a073c166fe780bcd
Andres De Jesus Cabrera <ajc2101@gmail.com>
2019-02-04 14:21:20 -0500

    single item query

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import Employees from "./components/Employees";
+// import Employees from "./components/Employees";
 import Employee from "./components/Employee";
 
 class App extends Component {
   render() {
     return (
-      <Employees />
+      <Employee />
     );
   }
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-
-
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 /*
 
 const Employee = {
@@ -25,26 +25,52 @@ const Employee = gql`
 }
 
 <Query query={Employee}>
-            {({ loading, error, data }) => {
-            if (loading) return <div>Fetching</div>
-            if (error) return <div>Error</div>
-
-            const EmployeeToRender = data.employee
-
-            return (
-                <div>
-                {EmployeeToRender.map(employee => <Employee key={employee.id} fullName={employee.fullName} />)}
-                </div>
-            )
-            }}
-        </Query>
+{({ loading, error, data }) => {
+  if (loading) return <div>Fetching</div>
+  if (error) return <div>Error</div>
+  
+  const EmployeeToRender = data.employee
+  
+  return (
+    <div>
+    {EmployeeToRender.map(employee => <Employee key={employee.id} fullName={employee.fullName} />)}
+    </div>
+    )
+  }}
+  </Query>
 */
 export default class Employee extends Component {
   render() {
+    const EmployeeQuery = gql`
+    {
+      employee(id: "cjrnnfvx4006v01pd02v6kwvf"){
+        id,
+        fullName,
+        currentJob{
+          title
+        },
+        goalJob{
+          title
+        }
+      }
+    }`
     return (
-      <div>
-        {this.props.fullName}
-      </div>
-    )
-  }
+      <Query query={EmployeeQuery}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>
+        if (error) return <div>Error</div>
+
+        const EmployeeToRender = data.employee
+
+        return (
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{EmployeeToRender.fullName}</h5>
+                <p className="card-text">{EmployeeToRender.currentJob.title}</p>
+                <p className="card-text">{EmployeeToRender.goalJob.title}</p>
+              </div>
+            </div>
+            )
+          }}
+          </Query>)}
 }
